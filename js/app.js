53e38dddaa50d238400a5e8660c4a862f195d34e
Bohdan <Telanim@outlook.com>
2025-06-10 15:53:18 +0300

    Starting point of application

---
+++ js/app.js
@@ -0,0 +1,23 @@
+import { Gallery } from "./gallery.js";
+import { UIManager } from "./UIManager.js";
+
+class ImageGalleryApp {
+  constructor() {
+    this.gallery = null;
+    this.uiManager = null;
+
+    this.init();
+  }
+
+  init() {
+    console.log('initialization');
+    this.gallery = new Gallery();
+    this.uiManager = new UIManager(this.gallery);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.ImageGalleryApp = new ImageGalleryApp();
+
+});
